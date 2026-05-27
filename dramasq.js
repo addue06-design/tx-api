6ebb850dd3f3b5b9373491eef27b847eb8dcc107
addue06-design <addue.06@gmail.com>
2026-05-27 17:41:51 +0800

    Update dramasq.js

--- dramasq.js
+++ dramasq.js
@@ -1,28 +1,27 @@
 var rule = {
-    title: 'DramasQ',
-    host: 'https://dramasq.com.tr',
+    title:'DramasQ',
+    host:'https://dramasq.com.tr',
 
-    url: '/vodshow/fyclass--------fypage---.html',
-    searchUrl: '/vodsearch/**----------fypage---.html',
+    url:'/vodshow/fyclass--------fypage---.html',
+    searchUrl:'/vodsearch/**----------fypage---.html',
 
-    searchable: 2,
-    quickSearch: 1,
-    filterable: 0,
+    searchable:2,
+    quickSearch:1,
+    filterable:0,
 
-    class_name: '陸劇&台劇&韓劇&日劇&泰劇&港劇&美劇&動漫',
-    class_url: '13&14&15&20&21&22&16&4',
+    class_name:'陸劇&台劇&韓劇&日劇&泰劇&港劇&美劇&動漫',
+    class_url:'13&14&15&20&21&22&16&4',
 
-    play_parse: true,
+    play_parse:true,
 
-    lazy: `
-js:
+    lazy:`js:
 let html = request(input);
 
-let match = html.match(/var player_aaaa=({.*?})</);
+let m = html.match(/var player_aaaa=({.*?})</);
 
-if(match){
+if(m){
 
-    let data = JSON.parse(match[1]);
+    let data = JSON.parse(m[1]);
 
     input = {
         parse:0,
@@ -32,45 +31,22 @@ if(match){
             "Referer":"https://dramasq.com.tr/"
         }
     }
-
 }
 `,
 
-    推荐: '*',
-    double: true,
+    limit:6,
 
-    一级: `
-js:
-let d = [];
+    推荐:'.items&&li;a&&title;a&&data-original;.module-item-note&&Text;a&&href',
 
-pdfh = jsp.pdfh;
-pdfa = jsp.pdfa;
+    一级:'.items&&li;a&&title;a&&data-original;.module-item-note&&Text;a&&href',
 
-let list = pdfa(html, 'body .module-items .module-item');
-
-list.forEach(it => {
-
-    d.push({
-        title: pdfh(it, '.module-item-title&&Text'),
-        pic_url: pdfh(it, '.lazyload&&data-original'),
-        desc: pdfh(it, '.module-item-note&&Text'),
-        url: pdfh(it, 'a&&href')
-    });
-
-});
-
-setResult(d);
-`,
-
-    二级: {
-        title: 'h1&&Text',
-        img: '.lazyload&&data-original',
-        desc: '.module-info-item-content&&Text',
-        content: 'pre&&Text',
-
-        tabs: '.title:contains(片源)',
-
-        lists: '.items&&li'
-    },
+    二级:{
+        title:'h1&&Text',
+        img:'.lazyload&&data-original',
+        desc:'font&&Text',
+        content:'pre&&Text',
+        tabs:'.title',
+        lists:'.items:eq(#id)&&li'
+    }
 
 }
