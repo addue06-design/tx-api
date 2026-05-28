8da2fd2c726acae00d85ea9db05cd875c0cc15ce
addue06-design <addue.06@gmail.com>
2026-05-28 10:55:30 +0800

    Update dramasq.js

--- js/dramasq.js
+++ js/dramasq.js
@@ -1,141 +1,51 @@
 var rule = {
-    title:'DramasQ',
-    host:'https://www.dramasq.com.tr',
-    headers:{
-        'User-Agent':'Mozilla/5.0',
-        'Referer':'https://dramasq.com.tr/',
-        'Accept-Language':'zh-TW,zh;q=0.9'
+    title: 'DramasQ',
+    host: 'https://www.dramasq.com.tr',
+    homeUrl: '/',
+    url: '/vodshow/fyclass--------fypage---.html',
+    searchable: 2,
+    quickSearch: 1,
+    filterable: 0,
+
+    headers: {
+        'User-Agent': 'Mozilla/5.0'
     },
-    url:'/vodshow/fyclass--------fypage---.html',
-    searchUrl:'/vodsearch/**----------fypage---.html',
 
-    searchable:2,
-    quickSearch:1,
-    filterable:0,
+    class_name: '陸劇&台劇&韓劇&日劇&泰劇&港劇&美劇&動漫',
+    class_url: '13&14&15&20&21&22&16&4',
 
-    class_name:'陸劇&台劇&韓劇&日劇&泰劇&港劇&美劇&動漫',
-    class_url:'13&14&15&20&21&22&16&4',
+    play_parse: true,
+    lazy: '',
 
-    play_parse:true,
+    limit: 6,
 
-    lazy:`js:
-let html = request(input);
+    推荐: '.drama;a&&title;.imgflat&&style;.carousel-caption p&&Text;a&&href',
+    一级: '.drama;a&&title;.imgflat&&style;.carousel-caption p&&Text;a&&href',
 
-let match = html.match(/var player_aaaa=({.*?})</);
+    二级: {
+        title: '.catleader h1&&Text',
+        img: '.imgflat&&style',
+        desc: '.carousel-caption p&&Text',
+        content: 'body&&Text',
+        tabs: '.play_source_tab a',
+        lists: '.paly_list_btn:eq(#id) a'
+    },
 
-if(match){
+    搜索: '.drama;a&&title;.imgflat&&style;.carousel-caption p&&Text;a&&href',
 
-    let data = JSON.parse(match[1]);
+    图片替换: function (input) {
+        let m = input.match(/url\\((.*?)\\)/);
+        if (m && m[1]) {
+            let url = m[1]
+                .replace(/"/g, '')
+                .replace(/'/g, '');
 
-    input = {
-        parse:0,
-        url:data.url,
-        header:{
-            "User-Agent":"Mozilla/5.0",
-            "Referer":"https://dramasq.com.tr/"
+            if (url.startsWith('/')) {
+                url = HOST + url;
+            }
+
+            return url;
         }
+        return '';
     }
-}
-`,
-
-    limit:20,
-
-    推荐:`js:
-let d=[];
-
-pdfa(html,'.module-items .module-item').forEach(it=>{
-
-    d.push({
-        title:pdfh(it,'.module-item-title&&Text'),
-        pic_url:pd(it,'.module-item-pic&&img&&data-original',HOST),
-        desc:pdfh(it,'.module-item-note&&Text'),
-        url:pd(it,'a&&href',HOST)
-    });
-
-});
-
-setResult(d);
-`,
-
-    一级:`js:
-let d=[];
-
-pdfa(html,'.module-items .module-item').forEach(it=>{
-
-    d.push({
-        title:pdfh(it,'.module-item-title&&Text'),
-        pic_url:pd(it,'.module-item-pic&&img&&data-original',HOST),
-        desc:pdfh(it,'.module-item-note&&Text'),
-        url:pd(it,'a&&href',HOST)
-    });
-
-});
-
-setResult(d);
-`,
-
-    二级:`js:
-VOD = {};
-
-VOD.vod_name = pdfh(html,'h1&&Text');
-
-VOD.vod_pic = pd(html,'.module-item-pic&&img&&data-original',HOST);
-
-VOD.type_name = pdfh(html,'.module-info-tag-link:eq(0)&&Text');
-
-VOD.vod_content = pdfh(html,'pre&&Text');
-
-let tabs = pdfa(html,'.module-tab-item');
-
-let playmap = {};
-
-tabs.forEach((tab,i)=>{
-
-    let from = pdfh(tab,'body&&Text');
-
-    playmap[from] = [];
-
-    let list = pdfa(html,'.module-play-list:eq('+i+') a');
-
-    list.forEach(it=>{
-
-        let title = pdfh(it,'a&&Text');
-
-        let url = pd(it,'a&&href',HOST);
-
-        playmap[from].push(title + '$' + url);
-
-    });
-
-});
-
-VOD.vod_play_from = Object.keys(playmap).join('$$$');
-
-let urls = [];
-
-Object.keys(playmap).forEach(key=>{
-
-    urls.push(playmap[key].join('#'));
-
-});
-
-VOD.vod_play_url = urls.join('$$$');
-`,
-
-    搜索:`js:
-let d=[];
-
-pdfa(html,'.module-items .module-item').forEach(it=>{
-
-    d.push({
-        title:pdfh(it,'.module-item-title&&Text'),
-        pic_url:pd(it,'.module-item-pic&&img&&data-original',HOST),
-        desc:pdfh(it,'.module-item-note&&Text'),
-        url:pd(it,'a&&href',HOST)
-    });
-
-});
-
-setResult(d);
-`
 }
