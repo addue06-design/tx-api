81a5174738e1c6a26b7062d9ad5efdaad46b7803
addue06-design <addue.06@gmail.com>
2026-06-03 13:30:05 +0800

    Update mysq.js

--- mysq.js
+++ mysq.js
@@ -1,25 +1,79 @@
-import { _ } from './lib/cat.js';
+let host = 'https://dramasq.cc';
+let header = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' };
 
-var rule = {
-    title: '劇迷DramasQ',
-    host: 'https://dramasq.cc',
-    url: '/vodtype/fyclass-fypage.html',
-    searchUrl: '/vodsearch/**----------fypage---.html',
-    searchable: 1,
-    quickSearch: 1,
-    filterable: 0,
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-    },
-    timeout: 5000,
-    class_name: '韓劇&陸劇&台劇&日劇&歐美劇',
-    class_url: 'hanju&dlju&twju&rbju&omju',
-    play_parse: true,
-    lazy: 'js: input = {parse: 1, url: input, header: rule.headers};',
-    double: true,
-    一级: 'js: var items = []; var html = request(input); var list = pdfa(html, ".list-vod li||.vodlist li||.pack-ykpack||.v-item"); list.forEach(function(it) { var title = pdfh(it, "a&&title||a&&text||.title&&text"); var img = pd(it, "img&&data-original||img&&src", input); var url = pd(it, "a&&href", input); var remarks = pdfh(it, ".remarks&&text||.pic-text&&text"); if (title && url) { items.push({ vod_name: title, vod_pic: img, vod_id: url, vod_remarks: remarks }); } }); setResult(items);',
-    二级: 'js: var html = request(input); var title = pdfh(html, ".vod-detail-info h1&&text||h1&&text"); var img = pd(html, ".vod-detail-pic img&&src||img&&src", input); var desc = pdfh(html, ".vod-detail-content&&text||.content&&text"); var remarks = pdfh(html, ".vod-detail-info span&&text"); var vod = { vod_id: input, vod_name: title, vod_pic: img, vod_remarks: remarks, vod_content: desc }; var playlist = []; var playUrls = pdfa(html, ".playlist li a||.play-btn a||.urllist li a"); playUrls.forEach(function(it) { var name = pdfh(it, "a&&text||text"); var href = pd(it, "a&&href||href", input); if (href) { playlist.push(name + "$" + href); } }); vod.vod_play_from = "劇迷快線"; vod.vod_play_url = playlist.join("#"); setResult({list: [vod]});',
-    搜索: 'js: var items = []; var html = request(input); var list = pdfa(html, ".search-list li||.vodlist li"); list.forEach(function(it) { var title = pdfh(it, "a&&title||a&&text"); var img = pd(it, "img&&data-original||img&&src", input); var url = pd(it, "a&&href", input); if (title && url) { items.push({ vod_name: title, vod_pic: img, vod_id: url }); } }); setResult(items);'
-};
+async function init(ext) {
+    return String("");
+}
 
-export default { rule };
+async function home(filter) {
+    let classes = [
+        {"type_id": "hanju", "type_name": "韓劇"},
+        {"type_id": "dlju", "type_name": "陸劇"},
+        {"type_id": "twju", "type_name": "台劇"},
+        {"type_id": "rbju", "type_name": "日劇"},
+        {"type_id": "omju", "type_name": "歐美劇"}
+    ];
+    return JSON.stringify({ class: classes });
+}
+
+async function category(tid, pg, filter, extend) {
+    if (pg <= 0) pg = 1;
+    let url = host + '/vodtype/' + tid + '-' + pg + '.html';
+    let html = await req(url, { headers: header });
+    let items = [];
+    
+    // 直接調用你反編譯看到的 pdfa, pdfh, pd
+    let list = pdfa(html, '.list-vod li, .vodlist li');
+    list.forEach(it => {
+        items.push({
+            vod_id: pdfh(it, 'a&&href'),
+            vod_name: pdfh(it, 'a&&title'),
+            vod_pic: pd(it, 'img&&data-original'),
+            vod_remarks: pdfh(it, '.remarks&&text')
+        });
+    });
+    return JSON.stringify({ page: pg, pagecount: 99, limit: 20, total: 999, list: items });
+}
+
+async function detail(id) {
+    let url = id.startsWith('http') ? id : host + id;
+    let html = await req(url, { headers: header });
+    let vod = {
+        vod_id: id,
+        vod_name: pdfh(html, '.vod-detail-info h1&&text'),
+        vod_pic: pd(html, '.vod-detail-pic img&&src'),
+        vod_remarks: pdfh(html, '.vod-detail-info span&&text'),
+        vod_content: pdfh(html, '.vod-detail-content&&text')
+    };
+    
+    let playlist = [];
+    let playUrls = pdfa(html, '.playlist li a, .urllist li a');
+    playUrls.forEach(it => {
+        let name = pdfh(it, 'a&&text');
+        let href = pdfh(it, 'a&&href');
+        playlist.push(name + '$' + href);
+    });
+    
+    vod.vod_play_from = "劇迷線路";
+    vod.vod_play_url = playlist.join('#');
+    return JSON.stringify({ list: [vod] });
+}
+
+async function search(wd, quick, pg) {
+    let url = host + '/vodsearch/' + wd + '----------' + pg + '---.html';
+    let html = await req(url, { headers: header });
+    let items = [];
+    let list = pdfa(html, '.search-list li, .vodlist li');
+    list.forEach(it => {
+        items.push({
+            vod_id: pdfh(it, 'a&&href'),
+            vod_name: pdfh(it, 'a&&title'),
+            vod_pic: pd(it, 'img&&data-original')
+        });
+    });
+    return JSON.stringify({ list: items });
+}
+
+async function play(flag, id, flags) {
+    return JSON.stringify({ parse: 1, url: id, header: header });
+}
